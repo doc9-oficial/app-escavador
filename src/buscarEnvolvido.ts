b89cc30e219f7a658e8a7a6004d12d733eb68e87
Pedro Tolvo <tolvo.pedro@gmail.com>
2025-10-20 17:01:09 -0300

    fix: data

--- src/buscarEnvolvido.ts
+++ src/buscarEnvolvido.ts
@@ -126,10 +126,12 @@ function buildQueryParams(params: BuscarEnvolvidoParams): string {
 
   if (params.dataMinima) {
     queryParams.append("data_minima", params.dataMinima);
+    params.dataMinima = formatDate(params.dataMinima);
   }
 
   if (params.dataMaxima) {
     queryParams.append("data_maxima", params.dataMaxima);
+    params.dataMaxima = formatDate(params.dataMaxima);
   }
 
   return queryParams.toString();
@@ -199,6 +201,14 @@ async function buscarEnvolvido(params: BuscarEnvolvidoParams): Promise<void> {
   } catch (error: any) {
     console.log(docgo.result(false, null, error.message));
   }
+}
+
+function formatDate(dateStr: string): string {
+  const date = new Date(dateStr);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
 }
 
 export default buscarEnvolvido;
